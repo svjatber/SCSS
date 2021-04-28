f33e3b98697a66c4ad186ca41c183f8f1ee292c4
Admin <admin@Admins-MacBook-Pro.local>
2021-04-28 14:08:14 +0300

    basic scss

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="light">
+    <div class="main">
+      <p class="main__paragraph1">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores commodi corporis ducimus earum eos illum, modi. Asperiores corporis cum deserunt dolor earum ex expedita iusto labore numquam suscipit? In, officiis!
+      </p>
+      <p class="main__paragraph2">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores commodi corporis ducimus earum eos illum, modi. Asperiores corporis cum deserunt dolor earum ex expedita iusto labore numquam suscipit? In, officiis!
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang="scss">
+
+</style>
